f0f0758946284af49677f66efd6e66602517830d
Jan Valentik <jvalentik@sk.ibm.com>
2020-03-12 10:13:54 +0100

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Jan Valentik on 12/03/2020.
+//  Copyright © 2020 Cloudis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
